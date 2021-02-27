0bff50cde64a2403f02b77b5daa11ba4db4ec6b1
Mike Lu <mike@alphacogs.co.uk>
2021-02-27 16:41:56 +0000

    implement graphQL API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { API, graphqlOperation } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <AmplifySignOut />
-      My App
-    </div>
-  );
+const listTodos = `query listTodos {
+  listTodos {
+    items {
+      id
+      name
+      description
+    }
+  }
+}`;
+
+const addTodo = `mutation createTodo( $name: String! $description: String!) {
+  createTodo( input: {
+    name: $name
+    description: $description
+  }) {
+    id
+    name
+    description
+  }
+}`
+
+// function App() {
+class App extends Component {
+  todoMutation = async () => {
+    const todoDetails = {
+      name: 'Party tonight!',
+      description: 'Amplify CLI rocks!'
+    };
+    const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newTodo));
+  };
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <AmplifySignOut />
+        <p> Click a button </p>
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App, true);
